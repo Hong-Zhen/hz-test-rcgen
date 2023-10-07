910723b816dc937d80c17d33c984ca6280bdae40
Hong Zhen <zhen.hong@ncs.com.sg>
2023-10-07 17:54:03 +0800

    edited test case

--- cypress/integration/random_test1.spec.js
+++ cypress/integration/random_test1.spec.js
@@ -1,6 +1,5 @@
-describe("home page", () => {
-    it("the h1 contains the correct text", () => {
-      cy.visit(URL)
-      cy.get("h1").contains("itchen")
+describe("Test 1", () => {
+    it("visit only", () => {
+      cy.visit("/")
     })
 })
