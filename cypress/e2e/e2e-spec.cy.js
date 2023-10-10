7a9b5d4e01679415ab7d19ac4e1821e726aad591
Hong Zhen <zhen.hong@ncs.com.sg>
2023-10-10 19:25:18 +0800

    Added simple test case

---
+++ cypress/e2e/e2e-spec.cy.js
@@ -0,0 +1,64 @@
+// npx cypress run
+
+beforeEach(() => {
+  cy.visit('/')
+  cy.viewport('macbook-16')
+  cy.reload(true)
+  cy.get('article.modal').not('.active-model')
+})
+
+afterEach(() => {
+  cy.reload(true)
+})
+
+describe("Navigate to Menu Page and back to Home Page", () => {
+  it ('To Menu Page', () => {
+    cy.get('ul.navigation-menu.flex-row.pop-font').not('.active')
+    .get('li:nth-child(2)').contains("Menu").click()
+    cy.url().should('include', '/menu')
+    
+    cy.get('ul.navigation-menu.flex-row.pop-font').not('.active')
+    .get('li:nth-child(1)').contains("Home").click()
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq(Cypress.config().baseUrl)
+    })
+  })
+})
+
+// Reciepe 
+
+describe("Navigate to About Page and back to Home Page", () => {
+  it ('To About Page', () => {
+    cy.get('ul.navigation-menu.flex-row.pop-font').not('.active')
+    .get('li:nth-child(4)').contains("About").click()
+    cy.url().should('include', '/about')
+
+    cy.get('ul.navigation-menu.flex-row.pop-font').not('.active')
+    .get('li:nth-child(1)').contains("Home").click()
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq(Cypress.config().baseUrl)
+    })
+  })
+})
+
+describe("Navigate to Contact Page and back to Home Page", () => {
+  it ('To Contact Page', () => {
+    cy.get('ul.navigation-menu.flex-row.pop-font').not('.active')
+    .get('li:nth-child(5)').contains("Contact").click()
+    cy.url().should('include', '/contact')
+
+    cy.get('ul.navigation-menu.flex-row.pop-font').not('.active')
+    .get('li:nth-child(1)').contains("Home").click()
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq(Cypress.config().baseUrl)
+    })
+  })
+})
+
+describe("Open login page", () => {
+  it('Open and close login page', () => {
+    cy.get('div.passive-button-style').contains("Log in").click()
+    cy.get('button.close-modal-btn').click()
+  })
+})
+
