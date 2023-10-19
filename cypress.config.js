e1caeb42d69ef7986bacfac11ddecce52ee06bea
Hong Zhen <zhen.hong@ncs.com.sg>
2023-10-19 11:12:06 +0800

    Trigger rebuild

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://main.d8770hxk753d4.amplifyapp.com/',
+    // baseUrl: 'https://main.d21g241m8zr597.amplifyapp.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
